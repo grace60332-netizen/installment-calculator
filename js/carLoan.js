2ad38ff9c2696ab879b2220c2a44095a89f9b60d
grace <grace60332@gmail.com>
2026-06-30 14:00:07 +0800

    十位是-1

--- js/carLoan.js
+++ js/carLoan.js
@@ -73,8 +73,8 @@
         months
       });
 
-      subsidy = Math.max(0, Finance.ROUNDUP(subsidy, 1));
-      actualDisbursement = Finance.ROUNDUP(loan - subsidy, 0);
+      subsidy = Math.max(0, Finance.ROUNDUP(subsidy, -1));
+      actualDisbursement = loan - subsidy;
     }
 
     return {
